89d54c5d76cafb2022344b684386fe673b896397
Eduardo A. Soares <eduardoandre.mcc@gmail.com>
2024-09-26 10:12:29 -0300

    Create cluster-wsl-setup.sh

---
+++ cluster-wsl-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install kubectl
+echo "Installing kubectl..."
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl  # Clean up
+
+# Install AWS CLI
+echo "Installing AWS CLI..."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm awscliv2.zip  # Clean up
+rm -rf aws  # Clean up
+
+# Get Windows username
+WINDOWS_USERNAME=$(cmd.exe /c "echo %USERNAME%" | tr -d '\r')
+
+# Set environment variable in .bashrc
+echo "export AWS_SHARED_CREDENTIALS_FILE=/mnt/c/Users/$WINDOWS_USERNAME/.aws/credentials" >> ~/.bashrc
+
+# Test installations
+echo "Testing installations..."
+kubectl version --client
+aws --version
+
+echo "Installation complete. Please restart your terminal or run 'source ~/.bashrc' to apply changes."
