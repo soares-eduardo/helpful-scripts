1d59d491de1c82a5587607fb002eb34d77dcdb58
Eduardo A. Soares <eduardoandre.mcc@gmail.com>
2024-09-26 10:23:00 -0300

    Update cluster-wsl-setup.sh

--- cluster-wsl-setup.sh
+++ cluster-wsl-setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Install unzip
+sudo apt update
+sudo apt install -y unzip
+
 # Install kubectl
 echo "Installing kubectl..."
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
